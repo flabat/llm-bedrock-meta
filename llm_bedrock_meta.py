f004d757ab1afd9e3dc0bf230b0f4f7702a5037e
Kai <kaisai1122@gmail.com>
2024-05-06 08:30:18 +0700

    Update llm_bedrock_meta.py

--- llm_bedrock_meta.py
+++ llm_bedrock_meta.py
@@ -99,7 +99,7 @@ class BedrockLlama(llm.Model):
         if conversation is None:
 
             return f"""
-                    < | begin_of_text | > < |start_header_id | >system < |end_header_id | >\n\nYou are a helpful assistant < |eot_id | > < |start_header_id | >user < |end_header_id | >\n\n{prompt} < |eot_id | > < |start_header_id | >assistant < |end_header_id | >\n\n
+                    < | begin_of_text | > < |start_header_id | >system < |end_header_id | >\n\n{system_prompt}< |eot_id | > < |start_header_id | >user < |end_header_id | >\n\n{prompt} < |eot_id | > < |start_header_id | >assistant < |end_header_id | >\n\n
             """
 
         messages = []
